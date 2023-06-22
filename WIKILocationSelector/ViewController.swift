e101823c0b3ebb130fce01a491ad8a0bfc084646
Pierre Mouton <pierrem@Pierres-MBP-2.home>
2023-06-22 18:59:47 +0200

    Initial Project setup

---
+++ WIKILocationSelector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WIKILocationSelector
+//
+//  Created by Pierre Mouton on 22/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
