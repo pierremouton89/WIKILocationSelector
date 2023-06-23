afeb0776eba7fec33d73688fa654ebe7bcfdf7c6
Pierre Mouton <pierrem@Pierres-MBP-2.home>
2023-06-23 21:44:29 +0200

    feat: Introduced HTTPClient into main application

--- WIKILocationSelectorTests/Data/Service/HTTPClientTests.swift
+++ WIKILocationSelectorTests/Data/Service/HTTPClientTests.swift
@@ -6,37 +6,7 @@
 //
 
 import XCTest
-
-public protocol HTTPClient {
-    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
-    func get(from url: URL) async -> Result
-}
-
-public class URLSessionHTTPClient: HTTPClient {
-    
-    private let session: URLSession
-    
-    public init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    private struct UnexpectedValuesRepresentation: Error {}
-    
-    public func get(from url: URL) async -> HTTPClient.Result {
-        do {
-            let (data, response) = try await session.data(from: url)
-            if let response = response as? HTTPURLResponse {
-                return .success((data, response))
-            } else {
-                return .failure(UnexpectedValuesRepresentation())
-            }
-        } catch {
-            return .failure(error)
-        }
-    }
-    
-}
-
+@testable import WIKILocationSelector
 
 final class HTTPClientTests: XCTestCase {
 
