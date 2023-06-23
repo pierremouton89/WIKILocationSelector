3aa487323731018d3a39ac043feb14a12e14232d
Pierre Mouton <pierrem@Pierres-MBP-2.home>
2023-06-23 23:06:32 +0200

    tests: Implemented logic for populating locations screen

--- WIKILocationSelectorTests/Presentation/LocationsListViewModelTests.swift
+++ WIKILocationSelectorTests/Presentation/LocationsListViewModelTests.swift
@@ -8,23 +8,54 @@
 import XCTest
 @testable import WIKILocationSelector
 
+final class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    var value: T {
+        didSet {
+            DispatchQueue.main.async {
+                self.listener?(self.value)
+            }
+        }
+    }
+    init(_ value: T) {
+        self.value = value
+    }
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
 
 
 protocol LocationsListViewModel {
+    
+    var title: Box<String> { get }
+    var locations: Box<[Location]> { get }
+    var errorMessage: Box<String?> { get }
+    
     func loadContent() async
 }
 
 
 class LocationsListViewModelImplementation: LocationsListViewModel {
     
+    static let TITLE = "Locations"
     private let locationsRepository: LocationsRepository
+    private(set) var title = Box<String>(TITLE)
+    private(set) var locations  = Box<[Location]>([])
+    private(set) var errorMessage = Box<String?>(.none)
         
     init(locationsRepository: LocationsRepository){
         self.locationsRepository = locationsRepository
     }
         
     func loadContent() async {
-        _ = try? await self.locationsRepository.retrieveLocations()
+        do {
+            self.locations.value = try await self.locationsRepository.retrieveLocations()
+        } catch {
+            self.errorMessage.value = error.localizedDescription
+        }
     }
 
 }
@@ -34,7 +65,8 @@ final class LocationsListViewModelTests: XCTestCase {
     
     private func createSUT() -> (LocationsListViewModel, LocationsRepositorySpy) {
         let repository = LocationsRepositorySpy()
-        let viewModel = LocationsListViewModelImplementation(locationsRepository: repository)
+        let viewModel =  LocationsListViewModelImplementation(locationsRepository: repository)
+        trackForMemoryLeaks(viewModel)
         return(viewModel, repository)
     }
     
@@ -47,11 +79,102 @@ final class LocationsListViewModelTests: XCTestCase {
     func test_loadContent_executeRetrieveCallOnRepository() async {
         let (sut, repository) = createSUT()
         
+        repository.complete(with: [])
         await sut.loadContent()
         
         XCTAssertEqual(repository.receivedMessage, [.retrieve])
     }
+    
+    func test_loadContent_emptyListRetrievedFromRepositoryThenListIsPublishedToLocations() async {
+        let (sut, repository) = createSUT()
+        let expectedResult = [Location]()
+        repository.complete(with: expectedResult)
         
+        await sut.loadContent()
+        let expectation = XCTestExpectation(description: "Expected empty location list to return")
+        
+        sut.locations.bind { locations in
+            XCTAssertEqual(expectedResult, locations)
+            expectation.fulfill()
+        }
+        
+        await fulfillment(of: [expectation])
+    }
+    
+    func test_loadContent_populatedListRetrievedFromRepositoryThenListIsPublishedToLocations() async {
+        let (sut, repository) = createSUT()
+        
+        let item1 = Location(
+            name: "Location1",
+            latitude: 52.3547498,
+            longitude: 4.8339215
+        )
+        let item2 = Location(
+            name: "Copenhagen",
+            latitude: 55.6713442,
+            longitude: 12.523785
+        )
+        let item3 = Location(
+            name: "Location3",
+            latitude: 40.4380638,
+            longitude: 3.7495758
+        )
+        
+        let expectedResult = [item1, item3 ,item2]
+        repository.complete(with: expectedResult)
+        
+        await sut.loadContent()
+        let expectation = XCTestExpectation(description: "Expected location list to return")
+        
+        sut.locations.bind { locations in
+            XCTAssertEqual(expectedResult, locations)
+            expectation.fulfill()
+        }
+        
+        await fulfillment(of: [expectation], timeout: 1)
+    }
+    
+    func test_loadContent_whenErrorIsRetrievedFromRepositoryEmptyListIsPublishedToLocations() async {
+        let (sut, repository) = createSUT()
+        
+        repository.complete(with: anyNSError())
+        
+        await sut.loadContent()
+        
+        let expectation = XCTestExpectation(description: "Expected location list to return")
+        sut.locations.bind { locations in
+            XCTAssertEqual([Location](), locations)
+            expectation.fulfill()
+        }
+        await fulfillment(of: [expectation], timeout: 1)
+    }
+    
+    func test_loadContent_whenErrorIsRetrievedFromRepositoryEmptyListIsPublishedToErrorMessage() async {
+        let (sut, repository) = createSUT()
+        
+        let error = anyNSError()
+        repository.complete(with: error)
+        
+        await sut.loadContent()
+        
+        let expectation = XCTestExpectation(description: "Expected location list to return")
+        sut.errorMessage.bind { message in
+            XCTAssertEqual(anyNSError().localizedDescription, message)
+            expectation.fulfill()
+        }
+        await fulfillment(of: [expectation], timeout: 1)
+    }
+    
+    func test_whenBindingToTitle_publishesTitle() async {
+        let (sut, _) = createSUT()
+        
+        let expectation = XCTestExpectation(description: "Expected title to be published")
+        sut.title.bind { title in
+            XCTAssertEqual(LocationsListViewModelImplementation.TITLE, title)
+            expectation.fulfill()
+        }
+        await fulfillment(of: [expectation], timeout: 1)
+    }
 }
 
 class LocationsRepositorySpy: LocationsRepository {
@@ -60,10 +183,26 @@ class LocationsRepositorySpy: LocationsRepository {
         case retrieve
     }
     private(set) var receivedMessage = [ReceivedMessage]()
+    private var results = [Result<[Location],Error>]()
+    private var count = 0
     
     func retrieveLocations() async throws -> [Location] {
         receivedMessage.append(.retrieve)
-        return []
+        let result = results[count]
+        self.count += 1
+        switch(result) {
+        case .success(let locations):
+            return locations
+        case .failure(let error):
+            throw error
+        }
     }
     
+    func complete(with locations: [Location],  at index: Int = 0) {
+        self.results.insert(.success(locations), at: index)
+    }
+    
+    func complete(with error: Error,  at index: Int = 0) {
+        self.results.insert(.failure(error), at: index)
+    }
 }
